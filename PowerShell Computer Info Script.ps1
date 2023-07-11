ee0fcb5601f4bba9524065345a1348bfd293b6cf
Amanda Evola <KrustyKrabMan1999@gmail.com>
2023-07-11 15:19:52 -0400

    Add files via upload

---
+++ PowerShell Computer Info Script.ps1
@@ -0,0 +1,13 @@
+﻿$systemInfo = Get-WmiObject -Class Win32_ComputerSystem
+$osInfo = Get-WmiObject -Class Win32_OperatingSystem
+
+$output = @"
+System Information:
+------------------
+Manufacturer: $($systemInfo.Manufacturer)
+Model: $($systemInfo.Model)
+Operating System: $($osInfo.Caption) $($osInfo.Version)
+Total Memory: $([math]::Round($systemInfo.TotalPhysicalMemory / 1MB, 2)) MB
+"@
+
+$output | Out-File -FilePath "SystemInfo.txt"
